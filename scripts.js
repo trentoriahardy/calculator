9ac2c53b2ee619fc66396b085a74ea949b6ad8e7
COR Students <corstudents@Matts-MBP-68.localdomain>
2018-11-05 11:24:40 -0500

    update files

--- scripts.js
+++ scripts.js
@@ -1,13 +1,7 @@
-const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
-  
-  function updateDisplay() {
-    const display = document.querySelector('.calculator-screen');
-    display.value = calculator.displayValue;
-  }
-  
-  updateDisplay();
+$(document).ready(function(){
+$(".calculatorbutton").click(function(){
+buttonClicked = this.innerHTML
+$(".calculatorInput").html(buttonClicked)
+})
+})
+
